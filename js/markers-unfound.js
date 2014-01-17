fb145e7738fb1492b9aec5f0c19d2b5436668c13
John Masterson <magnafix@gmail.com>
2014-01-16 16:12:10 -0800

    implemented florida political tweets

---
+++ js/markers-unfound.js
@@ -0,0 +1,186 @@
+var unfoundperson1 = L.marker([30.4385389,-84.26579736],{icon: grayIcon}).addTo(map);
+unfoundperson1.bindPopup("<i>We know what's #BestforFLA &acirc;Äú@FlChamber: The moral of @NoCasinos documentary &quot;Pushing Luck&quot;? If it's too good to be true....&acirc;Äù #sayfie</i><br><span class=littlegray>#1027366</span>")
+
+var unfoundperson2 = L.marker([28.57234616,-81.37258611],{icon: grayIcon}).addTo(map);
+unfoundperson2.bindPopup("<i>@FLGovScott I need to insist &amp;amp; suggest that FLSen @JoeNegronFL as your LTGov The ideal LTGov that will help on your quest to be reelected</i><br><span class=littlegray>#1028035</span>")
+
+var unfoundperson3 = L.marker([30.44026885,-84.28312406],{icon: grayIcon}).addTo(map);
+unfoundperson3.bindPopup("<i>And we're underway. Well over 100 in attendance. #sayfie #PushingLuck http://t.co/S9lADLisn2</i><br><span class=littlegray>#1027366</span>")
+
+var unfoundperson4 = L.marker([28.22676314,-82.67213599],{icon: grayIcon}).addTo(map);
+unfoundperson4.bindPopup("<i>@DavidJollyCD13 ad paints opponent as a Washington insider, but he's a lobbyist nd moved from DC.How does that work? @dccc @AlexSinkFlorida</i><br><span class=littlegray>#1028147</span>")
+
+var unfoundperson5 = L.marker([26.37672958,-80.06803914],{icon: grayIcon}).addTo(map);
+unfoundperson5.bindPopup("<i>Study shows that people recently enrolled in Medicaid visit ER 40% more than others. http://t.co/SZikcuCi4E  #sayfie</i><br><span class=littlegray>#1026706</span>")
+
+var unfoundperson6 = L.marker([28.42544641,-81.46141242],{icon: grayIcon}).addTo(map);
+unfoundperson6.bindPopup("<i>only at an RPOF conference do you overhear, &quot;I need to have a conversation with @PamBondi.&quot; #RPOF #TCOT #sayfie</i><br><span class=littlegray>#1027079</span>")
+
+var unfoundperson7 = L.marker([30.43564352,-84.30458874],{icon: grayIcon}).addTo(map);
+unfoundperson7.bindPopup("<i>Lol &acirc;Äú@politico: Alex Sink rises to top in Florida special election:http://t.co/EJh7kTVeKn via @POLITICOAlex&acirc;Äù</i><br><span class=littlegray>#1027397</span>")
+
+var unfoundperson8 = L.marker([28.4242549,-81.4651569],{icon: grayIcon}).addTo(map);
+unfoundperson8.bindPopup("<i>Giving @GOP report at the @FloridaGOP Annual Meeting. #GOP #Sayfie @lennycurry @FLGovScott @PamBondi http://t.co/S3DdTpOS19</i><br><span class=littlegray>#1027034</span>")
+
+var unfoundperson9 = L.marker([27.79277938,-82.65597882],{icon: grayIcon}).addTo(map);
+unfoundperson9.bindPopup("<i>Important read MT @fasanomike: A major issue in 2014 FL elections: Flood-insurance hikes ravage TB home sales http://t.co/mhm3BMk8NF #sayfie</i><br><span class=littlegray>#1026458</span>")
+
+var unfoundperson10 = L.marker([28.57235752,-81.37260217],{icon: grayIcon}).addTo(map);
+unfoundperson10.bindPopup("<i>@FLGovScott @LopezCantera another controversial FL Lt Gov office http://t.co/0laHpmSMpm</i><br><span class=littlegray>#1028035</span>")
+
+var unfoundperson11 = L.marker([27.83929552,-82.70110726],{icon: grayIcon}).addTo(map);
+unfoundperson11.bindPopup("<i>MT @mjmishak: Battle lines drawn: Dem @HouseMajPAC casts @AlexSinkFlorida as &quot;problem solver,&quot; @DavidJollyCD13 as corporate &quot;shill.&quot; #sayfie</i><br><span class=littlegray>#1026458</span>")
+
+var unfoundperson12 = L.marker([28.44915146,-81.39438442],{icon: grayIcon}).addTo(map);
+unfoundperson12.bindPopup("<i>@mattgaetz: we're diff than Congress, these R bipartisan bills - dont care if they come frm Dems or Repubs...its abt protecting kids #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson13 = L.marker([28.4252453,-81.4652916],{icon: grayIcon}).addTo(map);
+unfoundperson13.bindPopup("<i>Repping #FSU at the @FloridaGOP brunch w/ @FLGovScott - #GoNoles #GoGOP http://t.co/QEI6PP5zNY</i><br><span class=littlegray>#1027018</span>")
+
+var unfoundperson14 = L.marker([28.4252453,-81.4652916],{icon: grayIcon}).addTo(map);
+unfoundperson14.bindPopup("<i>Proud &amp;amp; honored to be w/fellow #GOP Floridian Women &amp;amp; @FLGovScott.  #ItsWorking @CindyGravesFL @FloridaGOP #sayfie http://t.co/lkwxqpgVir</i><br><span class=littlegray>#1027034</span>")
+
+var unfoundperson15 = L.marker([26.02387391,-80.12841084],{icon: grayIcon}).addTo(map);
+unfoundperson15.bindPopup("<i>&acirc;Äú@SteveCrisafulli: Florida needs strong, #Conservative leadership. Follow me if you will help me fight for it. #sayfie #tcot&acirc;Äù. No. #douche</i><br><span class=littlegray>#1026537</span>")
+
+var unfoundperson16 = L.marker([27.9411958,-82.4816277],{icon: grayIcon}).addTo(map);
+unfoundperson16.bindPopup("<i>Its been three months @FLGovScott and its still not working! Instead of bashing ACA's website, fix yours first! http://t.co/xNjjIHCOXK</i><br><span class=littlegray>#1028024</span>")
+
+var unfoundperson17 = L.marker([28.29559013,-81.93084604],{icon: grayIcon}).addTo(map);
+unfoundperson17.bindPopup("<i>@FLGovScott @willweatherford @DonGaetz @FLGOPMajority went 2 work turning Florida in2 a #PromiseZone several years back. #itsworking #sayfie</i><br><span class=littlegray>#1027077</span>")
+
+var unfoundperson18 = L.marker([28.09258773,-82.72037736],{icon: grayIcon}).addTo(map);
+unfoundperson18.bindPopup("<i>Merry Christmas from Rep. Debbie Mayfield! @debbie_mayfield  @SaintPetersblog #sayfie http://t.co/iiWL095jhV http://t.co/any8VK5DCp</i><br><span class=littlegray>#1024892</span>")
+
+var unfoundperson19 = L.marker([30.53176648,-87.22386489],{icon: grayIcon}).addTo(map);
+unfoundperson19.bindPopup("<i>I'm excited for the opportunities ahead for @StevePrecourt but @MyFLHouse &amp;amp; @FLGOPMajority will miss his leadership. #sayfie</i><br><span class=littlegray>#1027026</span>")
+
+var unfoundperson20 = L.marker([28.44915146,-81.39438442],{icon: grayIcon}).addTo(map);
+unfoundperson20.bindPopup("<i>@RonLBook testifying - avg sexual predator commits between 117 - 123 acts if sexual violence b4 theyre caught...they WILL reoffend #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson21 = L.marker([28.538335,-81.379237],{icon: grayIcon}).addTo(map);
+unfoundperson21.bindPopup("<i>RT @Tampa_Chamber: Tampa Chamber hosts @FLGovScott's visit with local business leaders -http://t.co/eVndUD9WgC #FloridaVanRentals</i><br><span class=littlegray>#1028049</span>")
+
+var unfoundperson22 = L.marker([28.67513637,-81.40922887],{icon: grayIcon}).addTo(map);
+unfoundperson22.bindPopup("<i>.@MurrenTom @terri_georgia @AlexSinkFlorida Let's hope the DNC gives her the support she needs this time! @DWStweets</i><br><span class=littlegray>#1027434</span>")
+
+var unfoundperson23 = L.marker([28.41534144,-81.57439919],{icon: grayIcon}).addTo(map);
+unfoundperson23.bindPopup("<i>@FLGovScott the @brandon_chamber wants you to come speak to our community.</i><br><span class=littlegray>#1028192</span>")
+
+var unfoundperson24 = L.marker([30.43970017,-84.28296031],{icon: grayIcon}).addTo(map);
+unfoundperson24.bindPopup("<i>great to have @TheDaraKam @newsservicefla here as well. #sayfie #PushingLuck</i><br><span class=littlegray>#1027366</span>")
+
+var unfoundperson25 = L.marker([29.26641294,-81.02980414],{icon: grayIcon}).addTo(map);
+unfoundperson25.bindPopup("<i>So they gov Rick Scott doesn't wanna legalize medical marijuana but they'll keep Oxycontin which resembles pharmaceutical-grade heroin.</i><br><span class=littlegray>#1027226</span>")
+
+var unfoundperson26 = L.marker([27.99179039,-82.69163095],{icon: grayIcon}).addTo(map);
+unfoundperson26.bindPopup("<i>I'm canvassing Safety Harbor and talking about how @AlexSinkFlorida is going to fight the gridlock in DC! http://t.co/8rYTSfMtmK</i><br><span class=littlegray>#1027435</span>")
+
+var unfoundperson27 = L.marker([28.44917639,-81.39420639],{icon: grayIcon}).addTo(map);
+unfoundperson27.bindPopup("<i>Thanks to @mattgaetz for his leadership on this vital legislation - with your help we WILL make FL safer #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson28 = L.marker([30.437876,-84.281889],{icon: grayIcon}).addTo(map);
+unfoundperson28.bindPopup("<i>The FL @naacp is ready to fight this attack on voters. #votersuppression @flgovscott  @floridaforall #sayfie http://t.co/RdfYQ4GDaT</i><br><span class=littlegray>#1027222</span>")
+
+var unfoundperson29 = L.marker([28.4240074,-81.4651231],{icon: grayIcon}).addTo(map);
+unfoundperson29.bindPopup("<i>Recent Tweet of @FLGovScott brunch pic made cameo in Twitter's @evins Tech Committee presentation at @FloridaGOP mtng! #RPOF #GOP #sayfie</i><br><span class=littlegray>#1027034</span>")
+
+var unfoundperson30 = L.marker([28.4491463,-81.39448257],{icon: grayIcon}).addTo(map);
+unfoundperson30.bindPopup("<i>@RonLBook: Federal statutes support civil commitment. Why? Bc sexual predators endanger our children, families, citizens #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson31 = L.marker([28.42347017,-81.46457028],{icon: grayIcon}).addTo(map);
+unfoundperson31.bindPopup("<i>Promises made.  Promises kept.  @FLGovScott #GOPWomen</i><br><span class=littlegray>#1027151</span>")
+
+var unfoundperson32 = L.marker([30.44009492,-84.29374737],{icon: grayIcon}).addTo(map);
+unfoundperson32.bindPopup("<i>#PushingLuck premieres at the IMAX Theatre in Tallahassee in just a few more minutes #sayfie http://t.co/MtaG24skIa</i><br><span class=littlegray>#1027366</span>")
+
+var unfoundperson33 = L.marker([27.7689967,-82.6810161],{icon: grayIcon}).addTo(map);
+unfoundperson33.bindPopup("<i>*sigh*..everytime i come home i attract white men that look like charlie crist and brothas that look like lawrence taylor....they both old</i><br><span class=littlegray>#1025378</span>")
+
+var unfoundperson34 = L.marker([25.95103694,-80.14165321],{icon: grayIcon}).addTo(map);
+unfoundperson34.bindPopup("<i>@RonLBook &quot;I want to clarify what was said here today: every expert has told us today that 100% of these people WILL reoffend&quot; #sayfie #SVP</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson35 = L.marker([28.44905188,-81.39408519],{icon: grayIcon}).addTo(map);
+unfoundperson35.bindPopup("<i>House Crim Justice Subcommittee discussing PCB 1404 -- PCB 1403 just passed favorably #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson36 = L.marker([28.4242549,-81.4651569],{icon: grayIcon}).addTo(map);
+unfoundperson36.bindPopup("<i>&quot;I have 2 words to start this meeting: Rick Scott&quot; - @lennycurry followed by applause at @FloridaGOP Annual Meeting. @FLGovScott #sayfie</i><br><span class=littlegray>#1027034</span>")
+
+var unfoundperson37 = L.marker([28.4492499,-81.39451367],{icon: grayIcon}).addTo(map);
+unfoundperson37.bindPopup("<i>@TravisJHutson just mentioned legislation being heard today would have saved #CherishPerrywinkle #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson38 = L.marker([28.4242549,-81.4651569],{icon: grayIcon}).addTo(map);
+unfoundperson38.bindPopup("<i>Rick Scott gives #NationalChampions #FSU shoutout at @FloridaGOP Annual Meeting! Chops from the crowd follow. @FLGovScott #GoNoles</i><br><span class=littlegray>#1027018</span>")
+
+var unfoundperson39 = L.marker([28.09313351,-82.37405908],{icon: grayIcon}).addTo(map);
+unfoundperson39.bindPopup("<i>Doggone. &acirc;Äú@fasanomike: HD 65 hopeful @ChrisSprowls breaks $100K raised threshold. http://t.co/Bg56oCrh4b #sayfie #forthelittleguyandgal&acirc;Äù</i><br><span class=littlegray>#1027011</span>")
+
+var unfoundperson40 = L.marker([28.44915146,-81.39438442],{icon: grayIcon}).addTo(map);
+unfoundperson40.bindPopup("<i>Why would we lengthen the statute of limitations vs eliminate? There is limit on suffering of victims #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson41 = L.marker([28.0960197,-81.9580795],{icon: grayIcon}).addTo(map);
+unfoundperson41.bindPopup("<i>How'd I miss this?? &quot;@FloridaGOP: The four phases of @CharlieCristFL at the #BCSChampionship last night. #sayfie http://t.co/3hFHkdEUln&quot;</i><br><span class=littlegray>#1027018</span>")
+
+var unfoundperson42 = L.marker([28.44911797,-81.39464074],{icon: grayIcon}).addTo(map);
+unfoundperson42.bindPopup("<i>Our president @RonLBook taking the podium on behalf of @LaurensKids #sayfie http://t.co/MjnEYkIsLu</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson43 = L.marker([26.45684629,-80.12586993],{icon: grayIcon}).addTo(map);
+unfoundperson43.bindPopup("<i>@lennycurry @FloridaGOP @CharlieCristFL is this a dictionary definition? Because I'm fairly certain you've described all politicians.</i><br><span class=littlegray>#1027999</span>")
+
+var unfoundperson44 = L.marker([27.13234333,-80.18536042],{icon: grayIcon}).addTo(map);
+unfoundperson44.bindPopup("<i>@FLGovScott it was cool to see you at Vista BLVD Orlando Star Bucks!</i><br><span class=littlegray>#1028106</span>")
+
+var unfoundperson45 = L.marker([26.1192707,-80.1367272],{icon: grayIcon}).addTo(map);
+unfoundperson45.bindPopup("<i>From the &quot;Brilliant&quot; file RT @SayfieReview: Times: #ChrisChristie to help #RickScott raise money in #Florida http://t.co/OgrEeDYSuX #sayfie</i><br><span class=littlegray>#1027066</span>")
+
+var unfoundperson46 = L.marker([30.4397007,-84.28375909],{icon: grayIcon}).addTo(map);
+unfoundperson46.bindPopup("<i>&quot;If you want to make money in a casino, own one.&quot; -Steve Wynn, casino exec #sayfie #PushingLuck</i><br><span class=littlegray>#1027366</span>")
+
+var unfoundperson47 = L.marker([28.43152683,-81.30616275],{icon: grayIcon}).addTo(map);
+unfoundperson47.bindPopup("<i>Your tax money at work. @ScottforFlorida proposes $100 million to advertise #Florida http://t.co/U72ewT7mOe</i><br><span class=littlegray>#1027380</span>")
+
+var unfoundperson48 = L.marker([30.2665781,-81.5063687],{icon: grayIcon}).addTo(map);
+unfoundperson48.bindPopup("<i>i see where your priorities lie, rick scott...</i><br><span class=littlegray>#1027169</span>")
+
+var unfoundperson49 = L.marker([25.83156,-80.12661],{icon: grayIcon}).addTo(map);
+unfoundperson49.bindPopup("<i>@FrancoRipple @MarcACaputo #sayfie #flgov Just Re-tweeted it!!</i><br><span class=littlegray>#1024582</span>")
+
+var unfoundperson50 = L.marker([30.45141717,-84.17688073],{icon: grayIcon}).addTo(map);
+unfoundperson50.bindPopup("<i>Love it! RT &acirc;Äú@AndyMarlette: #FSU cartoon: Tomahawk chop @FSU_Football @AuburnU @SEC #sayfie http://t.co/b8V2oh93DL&acirc;Äù</i><br><span class=littlegray>#1026791</span>")
+
+var unfoundperson51 = L.marker([30.44009492,-84.29374736],{icon: grayIcon}).addTo(map);
+unfoundperson51.bindPopup("<i>Good to see @MaryEllenKlas in attendance for the #PushingLuck premiere. #sayfie</i><br><span class=littlegray>#1027366</span>")
+
+var unfoundperson52 = L.marker([27.98112752,-82.53787911],{icon: grayIcon}).addTo(map);
+unfoundperson52.bindPopup("<i>@AlexSinkFlorida wishing you lots of luck and all our support!!</i><br><span class=littlegray>#1027411</span>")
+
+var unfoundperson53 = L.marker([28.45482705,-81.38634023],{icon: grayIcon}).addTo(map);
+unfoundperson53.bindPopup("<i>After my disclosure my abuser fled FL to OK &amp;amp; was found coaching 10 yr old girls. Sexually violent predators WILL reoffend #sayfie</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson54 = L.marker([43.16045352,-78.67271421],{icon: grayIcon}).addTo(map);
+unfoundperson54.bindPopup("<i>I want to see the Geordie Howe gta trick tonight #scott #sabres</i><br><span class=littlegray>#1025636</span>")
+
+var unfoundperson55 = L.marker([28.43147277,-81.30611106],{icon: grayIcon}).addTo(map);
+unfoundperson55.bindPopup("<i>Talking tourism: @ScottforFlorida announces $100 million for tourism @MCO #wftv http://t.co/obxpMowRXh</i><br><span class=littlegray>#1027380</span>")
+
+var unfoundperson56 = L.marker([25.76996661,-80.19879607],{icon: grayIcon}).addTo(map);
+unfoundperson56.bindPopup("<i>&acirc;Äú@FloridaGOP: Who supports #Obamacare? This guy --&amp;gt; @charliecristfl - and more than 2.1 million other people who have enrolled.</i><br><span class=littlegray>#1026704</span>")
+
+var unfoundperson57 = L.marker([28.0027304,-82.4459728],{icon: grayIcon}).addTo(map);
+unfoundperson57.bindPopup("<i>Great to see @AlexSinkFlorida crush it 2nite in #stpete. Best I've seen her on the stump; fired up &amp;amp; ready to win!! #sayfie #dccc</i><br><span class=littlegray>#1026957</span>")
+
+var unfoundperson58 = L.marker([28.44921059,-81.39446363],{icon: grayIcon}).addTo(map);
+unfoundperson58.bindPopup("<i>@LaurensKids making sure our President @RonLBook makes all our points to push legislation forward. #sayfie #LKallday http://t.co/gliAnfjJHC</i><br><span class=littlegray>#1027290</span>")
+
+var unfoundperson59 = L.marker([27.7812265,-82.6402559],{icon: grayIcon}).addTo(map);
+unfoundperson59.bindPopup("<i>Just saw a picture of Rick Scott giving an open mouthed smile and now I will never be able to sleep again.</i><br><span class=littlegray>#1028155</span>")
+
+var unfoundperson60 = L.marker([30.44026851,-84.28079595],{icon: grayIcon}).addTo(map);
+unfoundperson60.bindPopup("<i>Rep. Carlos Lopez-Cantera named new Lieutenant Governor of Florida.  Congratulations @FLGovScott on your new LG</i><br><span class=littlegray>#1027646</span>")
+
+var unfoundperson61 = L.marker([28.53801896,-81.3702189],{icon: grayIcon}).addTo(map);
+unfoundperson61.bindPopup("<i>Look what arrived in the mail for me today. @CharlieCristFL #freebook http://t.co/zJFOyD1hpq</i><br><span class=littlegray>#1027380</span>")
+
+var unfoundperson62 = L.marker([25.74161644,-80.29397682],{icon: grayIcon}).addTo(map);
+unfoundperson62.bindPopup("<i>@AlexSinkFlorida mixes religion with politics. Vote for someone else.</i><br><span class=littlegray>#1027547</span>")
+
